fcfc6aa898a9b91a0ca5d11888fdedf2110bb5c2
Guilherme Banaletti <guilherme_b95@live.com>
2026-05-04 20:21:06 -0300

    Final seção 3

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -2,12 +2,17 @@ import { test, expect } from '@playwright/test'
 import { faker } from '@faker-js/faker'
 
 test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
-    await request.delete('http://localhost:3333/helper/tasks/Ler um livro de TypeScript')
+   const taskName = 'Ler um livro de TypeScript'
+   await request.delete('http://localhost:3333/helper/tasks/' + taskName)
 
-    await page.goto('http://localhost:8080')
+   await page.goto('http://localhost:8080')
 
-    const inputTaskName = page.locator('input[class*=InputNewTask]')
-    await inputTaskName.fill('Ler um livro de TypeScript')
-    await page.click('css=button >> text=Create')
+   const inputTaskName = page.locator('input[class*=InputNewTask]')
+   await inputTaskName.fill(taskName)
+   await page.click('css=button >> text=Create')
+
+   const target = page.locator(`css=.task-item p >> text=${taskName}`)
+   await expect(target).toBeVisible()
+
 
 })
