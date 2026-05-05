9b0c56e11fad032c7d6600837d10d8172af9fa7c
Guilherme Banaletti <guilherme_b95@live.com>
2026-05-04 21:13:43 -0300

    testes independentes

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,18 +1,41 @@
 import { test, expect } from '@playwright/test'
 import { faker } from '@faker-js/faker'
 
+
 test('deve poder cadastrar uma nova tarefa', async ({ page, request }) => {
-   const taskName = 'Ler um livro de TypeScript'
-   await request.delete('http://localhost:3333/helper/tasks/' + taskName)
+    const taskName = 'Ler um livro de TypeScript'
+    await request.delete('http://localhost:3333/helper/tasks/' + taskName)
 
-   await page.goto('http://localhost:8080')
+    await page.goto('http://localhost:8080')
 
-   const inputTaskName = page.locator('input[class*=InputNewTask]')
-   await inputTaskName.fill(taskName)
-   await page.click('css=button >> text=Create')
+    const inputTaskName = page.locator('input[class*=InputNewTask]')
+    await inputTaskName.fill(taskName)
+    await page.click('css=button >> text=Create')
 
-   const target = page.locator(`css=.task-item p >> text=${taskName}`)
-   await expect(target).toBeVisible()
+    const target = page.locator(`css=.task-item p >> text=${taskName}`)
+    await expect(target).toBeVisible()
+
+
+})
+
+test.only('Não deve permitir tarefa duplicada', async ({ page, request }) => {
+    const task = {
+       name: 'Ler um livro de JavaScript',
+       is_done: false
+    }
+    await request.delete('http://localhost:3333/helper/tasks/' + task.name)
+
+    const newTask = await request.post('http://localhost:3333/tasks/', {data: task})
+    expect(newTask.ok()).toBeTruthy()
+
+    await page.goto('http://localhost:8080')
+
+    const inputTaskName = page.locator('input[class*=InputNewTask]')
+    await inputTaskName.fill(task.name)
+    await page.click('css=button >> text=Create')
+
+    const target = page.locator('.swal2-html-container')
+    await expect(target).toHaveText('Task already exists!')
 
 
 })
