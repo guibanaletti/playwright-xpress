81d9ca28a29f5d85ed37f11a8b57ec23815d021b
Guilherme Banaletti <guilherme_b95@live.com>
2026-05-19 14:02:17 -0300

    validar campo obrigatorio

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,4 +1,4 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { TaskModel } from './fixtures/task.model'
 import { deleteTaskByHelper, postTask } from './support/helpers'
 import { TasksPage } from './support/pages/tasks'
@@ -28,11 +28,25 @@ test('Não deve permitir tarefa duplicada', async ({ page, request }) => {
 
     await deleteTaskByHelper(request, task.name)
     await postTask(request, task)
-  
+
     const tasksPage: TasksPage = new TasksPage(page)
 
     await tasksPage.go()
     await tasksPage.create(task)
     await tasksPage.alertHaveText('Task already exists!')
 
+})
+
+test('Campo obrigatório', async ({ page }) => {
+    const task: TaskModel = {
+        name: '',
+        is_done: false
+    }
+    const tasksPage: TasksPage = new TasksPage(page)
+
+    await tasksPage.go()
+    await tasksPage.create(task)
+
+    const validationMessage = await tasksPage.inputTaskName.evaluate(e => (e as HTMLInputElement).validationMessage)
+    expect(validationMessage).toEqual('This is a required field')
 })
